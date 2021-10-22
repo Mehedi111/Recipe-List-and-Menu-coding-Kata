a5d94069aac92453820e051b54ba6d94680ee212
mehedi111 <ms.mehedi111@gmail.com>
2021-10-22 18:58:06 +0600

    [Task:2] Create skeleton recipes list activity

---
+++ task2/src/main/java/com/hellofresh/task2/ui/RecipesListActivity.kt
@@ -0,0 +1,12 @@
+package com.hellofresh.task2.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.hellofresh.task2.R
+
+class RecipesListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recipes_list)
+    }
+}
