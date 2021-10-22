55b66ef53e1986ba647c002990dd0bce3b189f51
mehedi111 <ms.mehedi111@gmail.com>
2021-10-22 23:10:20 +0600

    [Task:2] Add view binding and observe livedata

--- task2/src/main/java/com/hellofresh/task2/ui/home/RecipesListActivity.kt
+++ task2/src/main/java/com/hellofresh/task2/ui/home/RecipesListActivity.kt
@@ -2,11 +2,51 @@ package com.hellofresh.task2.ui.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.hellofresh.task2.R
+import androidx.activity.viewModels
+import com.hellofresh.task2.databinding.ActivityRecipesListBinding
+import com.hellofresh.task2.state.ViewState
+import dagger.hilt.android.AndroidEntryPoint
+import timber.log.Timber
 
+
+@AndroidEntryPoint
 class RecipesListActivity : AppCompatActivity() {
+
+    companion object {
+        private const val TAG = "RecipesListActivity_LOG"
+    }
+
+    private lateinit var binding: ActivityRecipesListBinding
+    private val viewModel: RecipesListViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_recipes_list)
+        binding = ActivityRecipesListBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        observeViewState()
+        requestRecipes()
+    }
+
+    private fun requestRecipes() {
+        viewModel.getRecipesList()
+    }
+
+    private fun observeViewState() {
+        viewModel.recipesListviewState.observe(this, { state ->
+            when (state) {
+                is ViewState.Loading -> {
+                    Timber.tag(TAG).d("Loading...")
+                }
+
+                is ViewState.Success -> {
+                    Timber.tag(TAG).d("Success...${state.data.size}")
+                }
+
+                is ViewState.Error -> {
+                    Timber.tag(TAG).d("Error...${getString(state.message)}")
+                }
+            }
+        })
     }
 }
