d72515db4f180e048f0ff04e366cb530175e55a1
mehedi111 <ms.mehedi111@gmail.com>
2021-10-23 00:06:40 +0600

    [Task:2] Implement function to show Snackbar

--- task2/src/main/java/com/hellofresh/task2/ui/BaseActivity.kt
+++ task2/src/main/java/com/hellofresh/task2/ui/BaseActivity.kt
@@ -1,6 +1,10 @@
 package com.hellofresh.task2.ui
 
+import android.graphics.Color
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
+import com.hellofresh.task2.R
 import com.hellofresh.task2.utils.network.NetworkUtils
 
 internal open class BaseActivity : AppCompatActivity() {
@@ -10,6 +14,19 @@ internal open class BaseActivity : AppCompatActivity() {
             .observe(this, { isConnected ->
                 callback(isConnected)
             })
+    }
+
+
+    internal fun showSnackBar(parentLayout: View?, message: String) {
+        parentLayout?.let {
+            Snackbar.make(it, message, Snackbar.LENGTH_LONG).apply {
+                setAction(getString(R.string.text_ok)) {}
+                setActionTextColor(Color.WHITE)
+                show()
+            }
+
+        }
+
     }
 
 }
